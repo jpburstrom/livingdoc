9ae5a9a9a27d66163c19e5a6a07f049a9080482d
Johannes Burström <johannes@ljud.org>
2017-11-10 12:50:58 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function ask_input() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+#sudo cp 11-automount-usb.rules /etc/udev/rules.d/
+sudo cp system/*.service /etc/systemd/system/
+sudo systemctl enable node-server
+
+#if [[ "yes" == $(ask_input "Install USB mounting rules?") ]] 
+#then
+#    sudo systemctl enable on-usb-mount
+#fi
+
+#if [[ "yes" == $(ask_input "Install autoplay?") ]] 
+#then
+#    sudo systemctl enable autoplay
+#fi
