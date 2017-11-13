fc3dd00ec04fe15997f8394b128550905ca58ce5
Johannes Burström <johannes@ljud.org>
2017-11-13 18:15:27 +0000

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-alias ro='sudo mount -o remount,ro /'
-alias rw='sudo mount -o remount,rw /'
+function ro {
+    sudo mount -o remount,ro /
+}
+function rw {
+    sudo mount -o remount,rw /
+}
+
 cd `dirname $0`
-if [ -f /tmp/updateme ]; then
+if [ -f /boot/updateme ]; then
     rw && ./pull.sh && ./install.sh
     ro
 fi
