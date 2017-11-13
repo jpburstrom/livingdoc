57c886fe77803e6269899b48c77bada27fc3ff05
Johannes Burström <johannes@ljud.org>
2017-11-13 21:37:21 +0000

    Fix lotta keyboard script

--- setup/lotta-keyboard/ledmatrix/printer.py
+++ setup/lotta-keyboard/ledmatrix/printer.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 
 import sys, time
+import os
 from rgbmatrix import Adafruit_RGBmatrix
 
 class Printer:
     def __init__(self):
         self.font_width = 6
         self.font_height = 10
+        self.max_char_width = 10
+        self.max_char_height = 3
         self.matrix = Adafruit_RGBmatrix(32, 2)
-        self.fontfile = "fonts/{0}x{1}.bdf".format(self.font_width, self.font_height)
-        self.offset = (0, 0)
+        self.fontfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "fonts/{0}x{1}.bdf".format(self.font_width, self.font_height))
+        self.offset = [0, 0]
 
     def offset_x(self ):
         return self.offset[0] * self.font_width
 
-    def offset_y(self, offset):
+    def offset_y(self):
         return self.offset[1] * self.font_height
 
     def draw_char(self, char):
@@ -31,3 +34,9 @@ class Printer:
         if self.offset[1] > self.max_char_height:
             self.offset[1] = 0
             self.matrix.Clear()
+
+
+if __name__ == '__main__':
+    printer = Printer()
+    printer.draw_char('x')
+
