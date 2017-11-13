ad44be5890838675ca906c1793cbbdd26f072031
Johannes Burström <johannes@ljud.org>
2017-11-13 21:58:13 +0100

    Add led keyboard stub

--- setup/lotta-keyboard/ledmatrix/printer.py
+++ setup/lotta-keyboard/ledmatrix/printer.py
@@ -3,15 +3,31 @@
 import sys, time
 from rgbmatrix import Adafruit_RGBmatrix
 
-font_width  = 4
-font_height = 6
-lines = ["Hi Mom!", "I'm cool."]
+class Printer:
+    def __init__(self):
+        self.font_width = 6
+        self.font_height = 10
+        self.matrix = Adafruit_RGBmatrix(32, 2)
+        self.fontfile = "fonts/{0}x{1}.bdf".format(self.font_width, self.font_height)
+        self.offset = (0, 0)
 
-matrix = Adafruit_RGBmatrix(16, 1)
-bdf_font_file = "fonts/{0}x{1}.bdf".format(font_width, font_height)
-matrix.DrawText(bdf_font_file, 1, 1, 0xFF, 0xFF, 0, lines[0])
-time.sleep(2.0)
-matrix.DrawText(bdf_font_file, 1, 1 + font_height, 0xFF, 0, 0xFF, lines[1])
-time.sleep(10.0)
+    def offset_x(self ):
+        return self.offset[0] * self.font_width
 
-matrix.Clear()
+    def offset_y(self, offset):
+        return self.offset[1] * self.font_height
+
+    def draw_char(self, char):
+        # If return, set offset to new line
+        if ord(char) == 10:
+            self.offset[0] += 1
+            self.offset[1] = 0
+        else:
+            self.matrix.DrawText(self.fontfile, self.offset_x(), self.offset_y(), 0xFF, 0xFF, 0xFF, char)
+            self.offset[0] += 1
+            if self.offset[0] > self.max_char_width:
+                self.offset[0] = 0
+                self.offset[1] += 1
+        if self.offset[1] > self.max_char_height:
+            self.offset[1] = 0
+            self.matrix.Clear()
