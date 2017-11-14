cf26125b89b0bdabc0750fd952a465b4bb3453e1
Johannes Burström <johannes@ljud.org>
2017-11-14 13:11:40 +0000

    restart services on update

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,6 +9,6 @@ function rw {
 
 cd `dirname $0`
 if [ -f /boot/updateme ]; then
-    rw && ./pull.sh && ./install.sh
+    rw && ./pull.sh && ./install.sh && sudo systemctl restart autoplay && sudo systemctl restart custom-shell && sudo systemctl restart node-server
     ro
 fi
