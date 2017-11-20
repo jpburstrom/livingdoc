3f5132d2e7cbfacf61101fef2b763a178bca51ba
Johannes Burström <johannes@ljud.org>
2017-11-20 20:42:48 +0000

    Change button input to mouse for some reason

--- setup/lotta-helmet/button-osc.py
+++ setup/lotta-helmet/button-osc.py
@@ -2,12 +2,12 @@
 
 import sys
 from time import sleep
-from inputs import get_key, UnpluggedError
+from inputs import get_mouse, UnpluggedError
 from liblo import send
 
 while 1:
     try:
-        events = get_key()
+        events = get_mouse()
         for event in events:
             if event.ev_type == 'Key' and event.state == 1:
                 send(57120, "/trig")
