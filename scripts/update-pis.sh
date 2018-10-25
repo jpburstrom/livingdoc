8bdd5d721ad99be22548264578cae349659c7641
Johannes Burström <johannes@ljud.org>
2018-10-25 16:09:35 +0200

    Rsync samples on update

--- scripts/update-pis.sh
+++ scripts/update-pis.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+OTHERROOT=/home/pi/livingdoc
+
+
 # ssh to all pis and git pull
 ro='sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot'
 rw='sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot'
 
+cd `git rev-parse --show-toplevel`
+
 for pi in `arp -a | grep b8:27:eb | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`; do
     ssh pi@$pi /bin/bash << EOF
+    hostname &&
     $rw &&
-    cd /home/pi/livingdoc &&
-    git pull &&
+    cd $OTHERROOT &&
+    git pull
+EOF
+    for dir in setup/*/samples; do
+        rsync -a $dir pi@$pi:$OTHERROOT/$dir
+    done
+
+ssh pi@$pi /bin/bash << EOF
     sudo systemctl restart autoplay &&
     $ro
 EOF
