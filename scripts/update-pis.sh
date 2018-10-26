b285153991ee7dcad49fd8b7a4ebf1f2f15ab693
Johannes Burström <johannes@ljud.org>
2018-10-26 15:23:15 +0200

    Fix rsync slash thing

--- scripts/update-pis.sh
+++ scripts/update-pis.sh
@@ -17,7 +17,7 @@ for pi in `arp -a | grep b8:27:eb | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}
     git pull
 EOF
     for dir in setup/*/samples; do
-        rsync -a $dir pi@$pi:$OTHERROOT/$dir
+        rsync -a $dir/ pi@$pi:$OTHERROOT/$dir
     done
 
 ssh pi@$pi /bin/bash << EOF
